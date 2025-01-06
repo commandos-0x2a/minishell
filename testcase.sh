d9a1598f2fbeb8081a96649c2c0bd3bc53092691
Yacoub Altayeh (Home) <yacoub.altayeh@icloud.com>
2025-01-07 00:46:10 +0300

    add .sh to add color

---
+++ testcase.sh
@@ -0,0 +1,4 @@
+ []	| echo test|cat -e	# pipe with arg in same token
+ []	| echo test| cat -e	# pipe with arg in same token
+ []	| echo test |cat -e # pipe with arg in same token
+ []	| echo "\"		  " # Don't handle backslash read the subject
