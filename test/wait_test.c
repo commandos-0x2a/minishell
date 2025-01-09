41694062a960a4e477a770476547c3d87d59ff1d
Yacoub Altayeh (Home) <yacoub.altayeh@icloud.com>
2025-01-10 01:02:11 +0300

    split files

--- test/wait_test.c
+++ test/wait_test.c
@@ -1,21 +1,55 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void	signal_manager(pid_t process)
+{
+	if (fork() == 0)
+	{
+		sleep(1);
+		kill(process, SIGSTOP);
+		sleep(5);
+		kill(process, SIGCONT);
+		sleep(1);
+
+		exit(0);
+	}
+}
 
 int main()
 {
-	struct rusage	rusage;
-	pid_t			target;
+	pid_t pid;
 
-	target = fork();
-	if (target == 0)
+	if ((pid = fork()) == 0)
 	{
-		sleep(1);
-		char	*argv[] = {"ls", NULL};
-		execve("/bin/ls", argv, NULL);
-		return (0);
+		int i = 0;
+		while (i < 10)
+		{
+			printf("[%d] counter: %i\n", getpid(), i);
+			sleep(1);
+			i++;
+		}
+		exit(0);
 	}
-	printf("target pid: %d\n", target);
-	wait4(target, NULL, 0, &rusage);
-	printf("system time used (PL): %d\n", rusage.ru_stime.tv_usec);
+
+	pid_t	test;
+	int		wstatus;
+
+	signal_manager(pid);
+	while ((test = waitpid(pid, &wstatus, WNOHANG | WUNTRACED | WCONTINUED)) != -1) // WNOHANG   WUNTRACED
+	{
+		if (WIFEXITED(wstatus))
+			printf("%d: Exited %d\n", test, WEXITSTATUS(wstatus)); 
+		else if (WIFCONTINUED(wstatus))
+			printf("%d: Continued\n",  test); 
+		else if (WIFSTOPPED(wstatus))
+			printf("%d: Stopped %d\n", test, WSTOPSIG(wstatus)); 
+		else if (WIFSIGNALED(wstatus))
+			printf("%d: Signaled by %d\n", test , WTERMSIG(wstatus)); 
+		else
+			printf("%d: WOW\n", test);
+		sleep(1);
+	}
 }
