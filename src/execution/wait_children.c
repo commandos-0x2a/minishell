629517e98f66eeb508203705337c3304fb281478
Muhammad Kurkar <mkurkar@c2r6s7.42amman.com>
2025-01-10 19:32:30 +0300

    refactor wait_children function for clarity and improved variable naming

--- src/execution/wait_children.c
+++ src/execution/wait_children.c
@@ -3,38 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   wait_children.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaltayeh <yaltayeh@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: mkurkar <mkurkar@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 17:47:06 by yaltayeh          #+#    #+#             */
-/*   Updated: 2025/01/10 00:37:09 by yaltayeh         ###   ########.fr       */
+/*   Updated: 2025/01/10 19:31:41 by mkurkar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wait_children.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkurkar <mkurkar@student.42amman.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 17:47:06 by yaltayeh          #+#    #+#             */
+/*   Updated: 2025/01/10 19:31:27 by mkurkar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wait_children.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkurkar <mkurkar@student.42amman.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 17:47:06 by yaltayeh          #+#    #+#             */
+/*   Updated: 2025/01/10 19:30:55 by mkurkar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int    wait_children(int target_pid)
+int wait_children(int target_pid)
 {
-	int	wstatus = 0;
-	int	ret = 1;
-	int pid;
+	int	wait_status = 0;
+	int	exit_code = 1;
+	int child_pid;
 
-	while ((pid = waitpid(-1, &wstatus, WUNTRACED)) != -1)
+	while ((child_pid = waitpid(target_pid, &wait_status, WUNTRACED)) != -1)
 	{
-		if (pid == target_pid)
+		if (child_pid == target_pid)
 		{
-			if (WIFEXITED(wstatus))
-				ret = WEXITSTATUS(wstatus);
-			else if (WIFSTOPPED(wstatus))
+			if (WIFEXITED(wait_status))
+				exit_code = WEXITSTATUS(wait_status);
+			else if (WIFSTOPPED(wait_status))
 			{
-				ft_fprintf(2, "pid: %d stopped\n", pid);
-				ret = 128 + WTERMSIG(wstatus);
+				ft_fprintf(2, "pid: %d stopped\n", child_pid);
+				exit_code = 128 + WTERMSIG(wait_status);
 				break;
 			}
-			else if (WIFSIGNALED(wstatus))
-				ret = 128 + WTERMSIG(wstatus);
+			else if (WIFSIGNALED(wait_status))
+				exit_code = 128 + WTERMSIG(wait_status);
 			else
-				ret = wstatus;
+			{
+				ft_fprintf(2, "Unexpected status for pid: %d\n", child_pid);
+				exit_code = wait_status;
+			}
 		}
 	}
-	return (ret);
+	return (exit_code);
 }
+
+
