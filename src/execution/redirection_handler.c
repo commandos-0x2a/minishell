954be2d7d55f3b66cd3982651601925179e87672
Muhammad Kurkar <mkurkar@c2r6s7.42amman.com>
2025-01-10 19:35:03 +0300

    refactor redirection error handling for improved clarity and consistency

--- src/execution/redirection_handler.c
+++ src/execution/redirection_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirection_handler.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaltayeh <yaltayeh@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: mkurkar <mkurkar@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 08:12:35 by yaltayeh          #+#    #+#             */
-/*   Updated: 2025/01/09 19:47:44 by yaltayeh         ###   ########.fr       */
+/*   Updated: 2025/01/10 19:34:22 by mkurkar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ int	in_redirection(char *token, int _dup)
 		return (-1);
 	if (ft_strcmp(file, "*") == 0)
 	{
-		ft_fprintf(2, NAME": %s: ambiguous redirect\n", token);
+		perror("ambiguous redirect");
 		free(file);
 		return (1);
 	}
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
 	{
-		perror(NAME);
+		ft_fprintf(2, NAME": %s\n", strerror(errno));
 		free(file);
 		return (-1);
 	}
@@ -51,14 +51,14 @@ int	out_append(char *token, int _dup)
 		return (-1);
 	if (ft_strcmp(file, "*") == 0)
 	{
-		ft_fprintf(2, NAME": %s: ambiguous redirect\n", token);
+		perror("ambiguous redirect");
 		free(file);
 		return (1);
 	}
 	fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (fd == -1)
 	{
-		perror(file);
+		ft_fprintf(2, NAME": %s\n", strerror(errno));
 		free(file);
 		return (-1);
 	}
@@ -86,7 +86,7 @@ int	out_redirection(char *token, int _dup)
 	fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 	{
-		perror(file);
+		ft_fprintf(2, NAME": %s\n", strerror(errno));
 		free(file);
 		return (-1);
 	}
@@ -107,7 +107,7 @@ int	redirection_handler(char **tokens, int _dup)
 		if (ft_strcmp(*tokens, "<<") == 0)
 			tokens++;
 		else if (ft_strncmp(*tokens, "<<", 2) == 0)
-			;
+			/* handle here-document redirection */;
 		else if (ft_strcmp(*tokens, "<") == 0)
 			status = in_redirection(*++tokens, _dup);
 		else if (ft_strncmp(*tokens, "<", 1) == 0)
