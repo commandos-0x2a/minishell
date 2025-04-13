79b41e90a235fbab017ba2b90365da4974370273
Yacoub Altayeh (42Amman) <yacoub.altayeh@icloud.com>
2025-04-13 17:54:00 +0300

    flow

--- src/execution/flow_control.c
+++ src/execution/flow_control.c
@@ -6,7 +6,7 @@
 /*   By: yaltayeh <yaltayeh@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 02:19:13 by yaltayeh          #+#    #+#             */
-/*   Updated: 2025/04/13 17:39:39 by yaltayeh         ###   ########.fr       */
+/*   Updated: 2025/04/13 17:42:12 by yaltayeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,11 @@ int	flow_control(t_mini *mini)
 	{
 		set_null_token(mini->tokens, &op);
 		if (test)
+		{
 			test = !pipeline_control(mini);
-		if (op == 2)
-			test = !test;
+			if (op == 2)
+				test = !test;	
+		}
 		if (op == 0)
 			break ;
 		tok_move2next(&mini->tokens);
