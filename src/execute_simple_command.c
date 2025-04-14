25e99303065918e0ff448367c4af185c7bef2ec2
KMKINGMAN <kingkurkar@gmail.com>
2025-04-14 15:24:29 +0300

    fix: update function parameter names for clarity and remove redundant code

--- src/execute_simple_command.c
+++ src/execute_simple_command.c
@@ -6,37 +6,33 @@
 /*   By: mkurkar <mkurkar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 21:59:26 by yaltayeh          #+#    #+#             */
-/*   Updated: 2025/04/14 15:21:36 by mkurkar          ###   ########.fr       */
+/*   Updated: 2025/04/14 15:23:44 by mkurkar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "minishell.h"
 
-static void	handle_cmd_execution(t_mini *mini, char **expand_argv)
+static void	handle_cmd_execution(t_mini *mini, char **argv)
 {
 	char	full_path[PATH_MAX];
 	int		err;
 
-	if (is_builtin(mini->env, expand_argv[0]))
-		handle_builtin(mini, expand_argv, 1);
-	err = get_full_path(mini->env, full_path, expand_argv[0]);
+	if (is_builtin(mini->env, argv[0]))
+		handle_builtin(mini, argv, 1);
+	err = get_full_path(mini->env, full_path, argv[0]);
 	if (err == 0)
 	{
-		execve(full_path, expand_argv, lst_2_dptr(mini->env));
+		execve(full_path, argv, lst_2_dptr(mini->env));
 		err = 1;
 	}
-	free_dptr(expand_argv);
+	free_dptr(argv);
 	mini_clean(mini);
 	exit(err);
 }
 
 void	execute_simple_command(t_mini *mini)
 {
-	char	**expand_argv;
-	char	**argv;
-	char		full_path[PATH_MAX];
-	int			err;
 	char		**argv;
 
 	if (argv_expander2(mini) != 0)
@@ -50,17 +46,17 @@ void	execute_simple_command(t_mini *mini)
 		PRINT_ALLOCATE_ERROR;
 		return ;
 	}
-	handle_cmd_execution(mini, expand_argv);
-	if (is_builtin(mini->env, argv[0]))
-		handle_builtin(mini, argv, 1);
+	handle_cmd_execution(mini, argv);
+	// if (is_builtin(mini->env, argv[0]))
+	// 	handle_builtin(mini, argv, 1);
 	
-	err = get_full_path(mini->env, full_path, argv[0]);
-	if (err == 0)
-	{
-		execve(full_path, argv, lst_2_dptr(mini->env));
-		err = 1;
-	}
-	free_dptr(argv);
-	mini_clean(mini);
-	exit(err);
+	// err = get_full_path(mini->env, full_path, argv[0]);
+	// if (err == 0)
+	// {
+	// 	execve(full_path, argv, lst_2_dptr(mini->env));
+	// 	err = 1;
+	// }
+	// free_dptr(argv);
+	// mini_clean(mini);
+	// exit(err);
 }
