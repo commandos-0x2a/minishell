0a95528d28f2fd09af5d246f3ac44b35c103a476
KMKINGMAN <kingkurkar@gmail.com>
2025-04-14 14:58:08 +0300

    refactor: extract command execution logic into a separate function

--- src/execute_simple_command.c
+++ src/execute_simple_command.c
@@ -3,21 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   execute_simple_command.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaltayeh <yaltayeh@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: mkurkar <mkurkar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 21:59:26 by yaltayeh          #+#    #+#             */
-/*   Updated: 2025/04/14 10:29:59 by yaltayeh         ###   ########.fr       */
+/*   Updated: 2025/04/14 14:57:58 by mkurkar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	handle_cmd_execution(t_mini *mini, char **expand_argv)
+{
+	char	full_path[PATH_MAX];
+	int		err;
+
+	if (is_builtin(mini->env, expand_argv[0]))
+		handle_builtin(mini, expand_argv, 1);
+	err = get_full_path(mini->env, full_path, expand_argv[0]);
+	if (err == 0)
+	{
+		execve(full_path, expand_argv, lst_2_dptr(mini->env));
+		err = 1;
+	}
+	free_dptr(expand_argv);
+	mini_clean(mini);
+	exit(err);
+}
+
 void	execute_simple_command(t_mini *mini)
 {
-	char		full_path[PATH_MAX];
-	int			err;
-	char		**expand_argv;
-	char		**argv;
+	char	**expand_argv;
+	char	**argv;
 
 	argv = lst_2_argv(&mini->tokens);
 	if (!argv)
@@ -32,17 +48,5 @@ void	execute_simple_command(t_mini *mini)
 		PRINT_ALLOCATE_ERROR;
 		return ;
 	}
-	// Check for built-in commands before getting full path and executing.
-	if (is_builtin(mini->env, expand_argv[0]))
-		handle_builtin(mini, expand_argv, 1);
-	
-	err = get_full_path(mini->env, full_path, expand_argv[0]);
-	if (err == 0)
-	{
-		execve(full_path, expand_argv, lst_2_dptr(mini->env));
-		err = 1;
-	}
-	free_dptr(expand_argv);
-	mini_clean(mini);
-	exit(err);
+	handle_cmd_execution(mini, expand_argv);
 }
